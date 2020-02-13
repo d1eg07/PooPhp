147e7d2397cfba93d263f33a711af2c07ee1ba1e
Diego <izayakazuto97@gmail.com>
2020-02-12 22:43:00 -0300

    Update conta.php

--- conta.php
+++ conta.php
@@ -5,4 +5,20 @@ class Conta //Criando Classe Conta
     public $cpfTitular; //Definindo atributos da classe conta
     public $nomeTitular;
     public $saldo;
+
+
+    public function sacar(float $valorSacar) //Método da classe conta
+    {
+            if($valorSacar >$this-> $saldo) //Usando referencia
+            {
+                echo "Saldo indisponível";
+            }    
+
+            else
+            {
+                $this->$saldo =- $valorSacar;
+            }
+    }
+
 }
+
